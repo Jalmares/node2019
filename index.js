0c7fa03495c641a478c84e3ffe14e953f248dac4
Jalmari <jalmares@metropolia.fi>
2019-11-15 14:32:00 +0200

    database connection in separate module exports

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 'use strict';
 
 const express = require('express');
+const connection = require('./module/db');
 
 const app = express();
 
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-	res.send('Hello from my Node server');
+app.get('/animal', async (req, res) => {
+	try {
+		const [results, fields] = await connection.query(
+			'SELECT * FROM animal');
+		console.log(results); //results contains rows r
+		console.log(fields); // fields contains extra m
+		res.json(results);
+	} catch (e) {
+		console.log(e);
+		res.send('db error :(');
+	}
 });
 
-app.get('/demo', (req, res) => {
-	res.send('this is demo');
-});
 
-app.listen(4000, () => {
-	console.log('server app start?');
-});
+	app.get('/demo', (req, res) => {
+		res.send('this is demo');
+	});
+
+	app.listen(4000, () => {
+		console.log('server app start?');
+	});
